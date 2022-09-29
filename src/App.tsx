148daeb1aa040197dd4235b7bb3018b3e89e5477
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-09-29 13:00:02 +0300

    structure of application

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      message: 'Hello Dima',
+      id: '2fsff3435',
+      user: { id: 'sdafeg', name: 'Dima' },
+    },
+    {
+      message: 'Hello Viktor',
+      id: '2fsf435435okdfkf3435',
+      user: { id: 'sdjhgjfhgfhafeg', name: 'Vlad' },
+    },
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <div
+          style={{
+            border: '1px solid black',
+            padding: '10px',
+            height: '300px',
+            width: '300px',
+            overflowY: 'scroll',
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          {messages.map(m => {
+            return (
+              <div key={m.id}>
+                <b>{m.user.name}:</b> {m.message}
+                <hr />
+              </div>
+            );
+          })}
+        </div>
+        <textarea></textarea>
+        <button>Send</button>
+      </div>
     </div>
   );
 }
