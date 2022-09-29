21bd938a094810b8c579ff7dc69839ba660cc049
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-09-29 16:09:32 +0300

    send messages on back

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { io } from 'socket.io-client';
+
+const socket = io('http://localhost:3009');
+// const socket = io('https://https://chat-w-s-back.herokuapp.com/');
+
+type MessageType = {
+  message: string,
+  id: string,
+  user: {
+    id: string,
+    name: string
+  }
+}
 
 function App() {
-  const [messages, setMessages] = useState([
-    {
-      message: 'Hello Dima',
-      id: '2fsff3435',
-      user: { id: 'sdafeg', name: 'Dima' },
-    },
-    {
-      message: 'Hello Viktor',
-      id: '2fsf435435okdfkf3435',
-      user: { id: 'sdjhgjfhgfhafeg', name: 'Vlad' },
-    },
-  ]);
+
+  const [messages, setMessages] = useState<Array<MessageType>>([]);
+  const [message, setMessage] = useState('hello');
+  const [name, setName] = useState<string>('');
+
+  useEffect( () => {
+    socket.on('init-messages-published', (messages: Array<MessageType>) => {
+      setMessages(messages)
+    })
+    socket.on('new-message-sent', (message: MessageType) => {
+      setMessages((messages) => [...messages, message] )
+    })
+  }, [] )
+
   return (
     <div className="App">
       <div>
@@ -35,8 +50,26 @@ function App() {
             );
           })}
         </div>
-        <textarea></textarea>
-        <button>Send</button>
+        <input value={name} onChange={e => setName(e.currentTarget.value)} />
+        <button
+          onClick={() => {
+            socket.emit('client-name-sent', name);
+          }}
+        >
+          Save name
+        </button>
+        <textarea
+          value={message}
+          onChange={e => setMessage(e.currentTarget.value)}
+        ></textarea>
+        <button
+          onClick={() => {
+            socket.emit('client-message-sent', message);
+            setMessage('');
+          }}
+        >
+          Send
+        </button>
       </div>
     </div>
   );
